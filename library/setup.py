349ce0dc57161a2d24fe21d8128f8e0be2339359
Phil Howard <phil@gadgetoid.com>
2018-02-15 12:44:53 +0000

    Prep for v0.1.0

--- library/setup.py
+++ library/setup.py
@@ -39,7 +39,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
 
 setup(
     name            = 'phatbeat',
-    version         = '0.0.2',
+    version         = '0.1.0',
     author          = 'Philip Howard',
     author_email    = 'phil@pimoroni.com',
     description     = """Python library for driving Pimoroni pHAT BEAT!""",
